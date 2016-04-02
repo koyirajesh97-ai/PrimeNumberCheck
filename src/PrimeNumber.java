60dc5cfd6c7461f37e493313627ab938fc592c15
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2016-04-02 13:50:22 +0000

    prime number check

---
+++ src/PrimeNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        boolean flag = false;
+        for(int i=2;i<=num/2;i++){
+            if(num % i == 0){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag && num > 1){
+            System.out.println(num + " is prime");
+        }else{
+            System.out.println(num + " is not prime");
+        }
+        sc.close();
+    }
+}
